00e3c34b826cb97de25a9e75c257a4fc459cbb96
DESKTOP-7VPLHCD\aa <won4413@daum.net>
2018-11-04 12:22:10 +0900

    index.php
    scandir 함수와 count함수 그리고 problem of scandir

--- php/index.php
+++ php/index.php
@@ -7,9 +7,20 @@
   <body>
     <h1><a href="index.php">WEB</a></h1>
     <ol>
-      <li><a href="index.php?id=HTML">HTML</a></li>
-      <li><a href="index.php?id=CSS">CSS</a></li>
-      <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+      <?php
+        $list = scandir('./data'); /* scandir 디렉토리를 읽어오는 함수 */
+        $i = 0;
+        while($i < count($list)){ /*count 길이를 알려주는 함수*/
+          // scandir의 문제점 배열의 0,1에 표현하지 않아도 될 값이 들어감 
+          // 다른표현  if($list[$i] != '.' && $list[$i] != '..') 
+          if($list[$i] != '.') { 
+            if($list[$i] != '..') {
+              echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+            }
+          }
+          $i = $i + 1;
+        }
+      ?>
     </ol>
     <h2>
       <?php
