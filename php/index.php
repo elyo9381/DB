b159188d18af271adfa617724fbcf6bdcb3071c0
DESKTOP-7VPLHCD\aa <won4413@daum.net>
2018-11-05 18:40:02 +0900

    index.php
    함수사용

--- php/index.php
+++ php/index.php
@@ -1,42 +1,55 @@
+<?php
+function print_title(){
+  if(isset($_GET['id'])){
+    echo $_GET['id'];
+  } else {
+    echo "Welcome";
+  }
+}
+function print_description(){
+  if(isset($_GET['id'])){
+    echo file_get_contents("data/".$_GET['id']);
+  } else {
+    echo "Hello, PHP";
+  }
+}
+function print_list(){
+  $list = scandir('./data');
+  $i = 0;
+  while($i < count($list)){
+    if($list[$i] != '.') {
+      if($list[$i] != '..') {
+        echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+      }
+    }
+    $i = $i + 1;
+  }
+}
+?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>
+      <?php
+      print_title();
+      ?>
+    </title>
   </head>
   <body>
     <h1><a href="index.php">WEB</a></h1>
     <ol>
       <?php
-        $list = scandir('./data'); /* scandir 디렉토리를 읽어오는 함수 */
-        $i = 0;
-        while($i < count($list)){ /*count 길이를 알려주는 함수*/
-          // scandir의 문제점 배열의 0,1에 표현하지 않아도 될 값이 들어감 
-          // 다른표현  if($list[$i] != '.' && $list[$i] != '..') 
-          if($list[$i] != '.') { 
-            if($list[$i] != '..') {
-              echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
-            }
-          }
-          $i = $i + 1;
-        }
+      print_list();
       ?>
     </ol>
     <h2>
       <?php
-      if(isset($_GET['id'])){
-        echo $_GET['id'];
-      } else {
-        echo "Welcome";
-      }
+      print_title();
       ?>
     </h2>
     <?php
-    if(isset($_GET['id'])){
-      echo file_get_contents("data/".$_GET['id']);
-    } else {
-      echo "Hello, PHP";
-    }
+    print_description();
      ?>
   </body>
 </html>
