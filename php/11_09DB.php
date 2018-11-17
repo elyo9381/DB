25fd039f1e1b0400165d9b6550aadf891d147e71
DESKTOP-7VPLHCD\aa <won4413@daum.net>
2018-11-17 15:47:55 +0900

    11_09DB.php
    php 코딩 홈페이지 만드는중 ( 이미지 게시판 만듬)

--- php/11_09DB.php
+++ php/11_09DB.php
@@ -28,17 +28,19 @@
 	</div>
 
 	<div id="header">
+
+		
 		
 			 <form action=<?=$PHP_SELF?> >      
-			<input type='hidden' name='id' value='<?=$id?>'>
+			<!-- <input type='hidden' name='id' value='<?=$id?>'> -->
 
 		  <tr>    
 		<td width='100%' height='30' align=right bgcolor='#FFFFFF'> 
 			<select name='Search_mode' style="width:80px; height:20px; color:#8A8B8B;  border:1px solid #BAC3F8; background:#CDFCEE;">
-		      <option value='3'> 전체에서
-			  <option value='1'> 상품명에서   
-		      <option value='2'> 홍보글에서      
-		    </select>  
+		      <option value='3'> 검색
+			  <option value='1'> 차량검색 
+		      <option value='2'> 차종검색     
+		    </select>   
 
 			
 
@@ -55,33 +57,119 @@
 		<div class="section_main">
 			
 		</div>
+
+		<table border='0'  width='100%'  align='center' valign='top' cellspacing='0' cellpadding='0' bgcolor='FFFFFF'>
+		<tr>
+		<td  height='10' align='center'  bgcolor='#FFFFFF' >&nbsp;</td>
+
+		<td height='10' align='center' bgcolor='#FFFFFF' >&nbsp;</td>
+		<td height='10' align='center' bgcolor='#FFFFFF' >&nbsp;</td>
+
+		<td height='10' align='center' bgcolor='#FFFFFF' >&nbsp;</td>
+		<td height='10' align='center' bgcolor='#FFFFFF' >&nbsp;</td>
+
+		<td height='10' align='center' bgcolor='#FFFFFF' >&nbsp;</td>
+		<td height='10' align='center' bgcolor='#FFFFFF' >&nbsp;</td>
+
+		<td height='10' align='center' bgcolor='#FFFFFF' >&nbsp;</td>
+		<td height='10' align='center' bgcolor='#FFFFFF' >&nbsp;</td>
+
 		
-		<div class="aside">
+
+
+		<tr>
+		<td width='4%' height='230' align='center' bgcolor='#FFFFFF' >&nbsp;</td>
+
+
+		
+
+		<?php
+
+		$Search_text=$_GET[Search_text];
+		$Search_mode=$_GET[Search_mode];
+
+		$where = 'passenger';
+		if($Search_text){
+			if($Search_mode==1) $tmp="model_name";
+			if($Search_mode==2) $tmp="car_kind";
+
+
+			if($Search_mode==3){
+			$where="model_name like '%$Search_text%' or car_kind like '%$Search_text%'";}
+			else{
+			$where="$tmp like '%$Search_text%'";  }
+			}?>
+
+
+			<?
+
+
+
+		include("./db_connect.php");
+		$conn=dbconn();
+		mysqli_query($conn,"set names utf8");
+		$cnt=1;
+	    $sql = "SELECT * FROM model where $where and passenger=5";
+	    $result = mysqli_query($conn, $sql);
+	    while($data = mysqli_fetch_array($result)){
+
+	    	// var_dump($data);
+	    ?>
+			
+	    
+			
+	    	<td width='20%' height='410' align='center' valign='top' bgcolor='#FFFFFF'>
+
+			
 			<table border='0' width='240' bgcolor='#FFFFFF' cellspacing='0' cellpadding='0' align='center' valign='middle'> 
+
 			<tr>
-   			<td width='180' height='230' align='center' valign='middle'>
-   			</td> 
- 
+   			 <td width='100%' height='auto' align='center' valign='middle'> 
 
-		  <tr> 
-		  <td width='230' height='80' align='center' valign='middle'> <!---__--->
-		</div>
+			<A  href= >
+			<img src='./image/<?=$data[image_path]?>' width='95%' height='AUTO' border='0' onmouseover='this.style.filter="alpha(opacity=60)"' onmouseout='this.style.filter=""'></a>
+			
+			</td>  				
+
+		 	<tr>  
+			<td width='100%' height='80' align='center' valign='middle'>
+
+			<a href= >
+			<span style='font-size:10pt; font-family:verdana,Tahoma; color:#6D6D6B'>
+			가격비교</span> </td>
+
+		   	<tr> 
+		   	<td width='100%' height='80' align='center' valign='middle'> 
+
+	   		<a href= >
+			<span style='font-size:10pt; font-family:verdana,Tahoma; color:#6D6D6B'>
+			<?=$subject=mb_substr($data[model_name],0,20,'UTF-8');  //문자열자르기; ?></span> </td>
+
+			<tr>
+			<td  height='4%'  align='center' bgcolor='#FFFFFF'>&nbsp;</td>
+			</tr>
+			</table> 
+
+			</td>
+			<td width='4%' align='center' bgcolor='#FFFFFF'>&nbsp;</td>
+
+
+			<? 
+				$cnt++;
+			   if($cnt%4==1) {
+				echo "<tr><td>";
+			 } } ?>
+
+			 </td>
+			</tr>
+			</table>
+
+		
+		
+
+
+			
+			
 	</div>
-	
-
-
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
